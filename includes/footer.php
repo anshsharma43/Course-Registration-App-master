b667eb6cbd331a95de614fecbdd1b0c8fb77afe8
Ansh Sharma <anshsharma.niku@gmail.com>
2021-08-28 15:42:54 +0530

    Initial commit

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    &copy; 2021 Online Course Registration | By : <a href="admin/index.php " target="_blank">Ansh Sharma</a>
+                </div>
+
+            </div>
+        </div>
+    </footer>
